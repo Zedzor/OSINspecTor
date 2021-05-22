4b2060331367efa22d60f12bd8167f9c50fe1565
danielfeitopin <danielfeitopin@protonmail.com>
2021-05-22 20:10:35 +0200

    Añadidos botones

--- OSINspecTor/app/utils/buttons.py
+++ OSINspecTor/app/utils/buttons.py
@@ -5,17 +5,28 @@ from app.model.ip.reverse import get_reverse
 
 
 class Buttons:
-    funcs = {
-        1: ('Subdominios', get_subdomains), #Dominios
-        2: ('Geolocalizar', get_geo), #Dominios e IP
-        3: ('DNS', None), #Dominios
-        4: ('MX', None), #Dominios
-        5: ('Emails', get_emails), #Dominios
-        6: ('Reverse', get_reverse), #IP
-        7: ('IP range', None), #Dominios
-        8: ('Vulns', None), #Dominios e IP
+    FUNCS = {
+        1: ('btn_sub', 'Subdominios', get_subdomains), #Dominios
+        2: ('btn_geo', 'Geolocalizar', get_geo), #Dominios e IP
+        3: ('btn_dns', 'DNS', None), #Dominios
+        4: ('btn_mx', 'MX', None), #Dominios
+        5: ('btn_em', 'Emails', get_emails), #Dominios
+        6: ('btn_rev', 'Reverse', get_reverse), #IP
+        7: ('btn_ran', 'IP range', None), #Dominios
+        8: ('btn_vul', 'Vulns', None), #Dominios e IP
     }
 
-    domain_funcs = (1, 2, 3, 4, 5, 6, 8)
+    DOMAIN_FUNCS = (1, 2, 3, 4, 5, 6, 8)
+    IP_FUNCS = (2, 6, 8)
 
-    ip_funcs = (2, 6, 8)
+    def _get_funcs(self, l: tuple):
+        funcs = []
+        for i in l:
+            funcs.append((self.FUNCS[i][0], self.FUNCS[i][1]))
+        return funcs
+
+    def get_domain_funcs(self):
+        return self._get_funcs((1,2,5))
+
+    def get_ip_funcs(self):
+        return self._get_funcs((2,6))
--- OSINspecTor/app/views.py
+++ OSINspecTor/app/views.py
@@ -8,25 +8,19 @@ def index(request: HttpRequest):
     return render(request, 'index.html')
 
 def dominio(request: HttpRequest):
-    buttons = []
-    for i in [1,2,5]: # Cambiar por Buttons.domain_funcs
-        buttons.append(Buttons.funcs[i])
 
     contex = {
         'cabecera': "Búsqueda por dominio",
         'form_label': 'Introduzca un dominio:',
-        'buttons': buttons,
+        'buttons': Buttons().get_domain_funcs(),
     }
     return render(request, 'busqueda.html', contex)
 
 def ip(request: HttpRequest):
-    buttons = []
-    for i in [2]: # Cambiar por Buttons.ip_funcs
-        buttons.append(Buttons.funcs[i])
 
     contex = {
         'cabecera': "Búsqueda por IP",
         'form_label': 'Introduzca una IP:',
-        'buttons': buttons,
+        'buttons': Buttons().get_ip_funcs(),
     }
     return render(request, 'busqueda.html', contex)
