62c7bd97316cb62e82281722f887252f3c81c010
p.serrano <p.serrano@udc.es>
2021-05-22 20:35:57 +0200

    geo rev y sub añadido a las vistas

--- OSINspecTor/app/views.py
+++ OSINspecTor/app/views.py
@@ -2,6 +2,9 @@ from django.http.request import HttpRequest
 from django.shortcuts import render
 from django.http import HttpResponse
 from app.utils.buttons import Buttons
+from app.model import get_geo
+from app.model.ip import get_reverse
+from app.model.domain import get_subdomains
 
 # Create your views here.
 def index(request: HttpRequest):
@@ -24,3 +27,28 @@ def ip(request: HttpRequest):
         'buttons': Buttons().get_ip_funcs(),
     }
     return render(request, 'busqueda.html', contex)
+
+def rev(request):
+    if request.method == 'POST':
+        ip = request.POST['ip']
+        resolucion = get_reverse(ip)
+        return resolucion
+    else:
+        return HttpResponse("Peticion no valida")
+
+def geo(request):
+    if request.method == 'POST':
+        ip = request.POST['ip']
+        localizacion = get_geo(ip)
+        return localizacion
+    else:
+        return HttpResponse("Peticion no valida")
+
+def sub(request):
+    if request.method == 'POST':
+        domain = request.POST['domain']
+        subdominios = get_subdomains(domain)
+        return subdominios
+    else:
+        return HttpResponse("Peticion no valida")
+    
