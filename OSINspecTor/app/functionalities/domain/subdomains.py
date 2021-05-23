67bc1f37ee9405e788c7370cb88d1e043a44d96b
danielfeitopin <danielfeitopin@protonmail.com>
2021-05-23 19:48:48 +0200

    Acabado backend de F1

--- OSINspecTor/app/functionalities/domain/subdomains.py
+++ OSINspecTor/app/functionalities/domain/subdomains.py
@@ -1,8 +1,30 @@
 import json
 import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
-def get_subdomains(dominio):
+def get_subdomains(dominio: str) -> dict:
+
+	def get_types_graph(df: pd.DataFrame):
+		types = df['type'].value_counts()
+		plt.pie(types, labels=types.index, autopct='%1.1f%%')
+		plt.title('Subdomain types')
+		plt.axis('equal')
+
+		with BytesIO() as buff:
+			plt.savefig(buff, format='png')
+			buff.seek(0)
+			figdata = base64.b64encode(buff.read())
+			return figdata
+
 	suburl = "https://sonar.omnisint.io/all/"+dominio
 	data = requests.get(suburl)
-	status = json.loads(data.text)
-	return status
+	df = pd.DataFrame(json.loads(data.text))
+	df.drop(columns=['subdomain', 'domain', 'tld'], inplace=True)
+	data = {
+		'graph': get_types_graph(df),
+		'df_dict': df.to_dict(),
+	}
+	return data
