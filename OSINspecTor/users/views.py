fa40eff1d553f6d5e7624b6acbf9714af4de0b91
pserrano99 <p.serrano@udc.es>
2021-06-11 21:56:23 +0200

    vistas de log y reg

--- OSINspecTor/users/views.py
+++ OSINspecTor/users/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from OSINspecTor.users.forms import LoginForm, SignupFrom
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
 
 # Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            contraseña = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=contraseña)
+            login(request, user)
+            return redirect('index')
+        return render(request, 'users/signup.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect(request.GET['next'])
+
+def index(request):
+    loginError=""
+
+    if 'username' in request.POST:
+        username=request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+        else:
+            loginError="Error de login"
+
+    loginForm = LoginForm()
+    signupForm = SignupFrom()
+
+    if request.user.is_authenticated:
+        context={'user':request.user, 'login_form':loginForm,'signup_form':signupForm,'loginError':loginError}
+    else:
+        context={'login_form':loginForm,'signup_form':signupForm,'loginError':loginError}
+    
+    return render(request, 'user/index-html', context)
