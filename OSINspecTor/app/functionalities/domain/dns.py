ace8196c014e481b79bed6014724a592f6c90486
nborregan <ignacio.borregan@udc.es>
2021-06-13 01:25:41 +0200

    Arreglado response code funcionalidad dns

--- OSINspecTor/app/functionalities/domain/dns.py
+++ OSINspecTor/app/functionalities/domain/dns.py
@@ -1,14 +1,23 @@
 import json
 import requests
+from django.http import JsonResponse
 
 
-def get_dns(domain):
-    domain_url = "https://freeapi.robtex.com/pdns/forward/"+domain
-    response = requests.get(domain_url)
-    if response.ok:
-        try:
-            return json.loads(response.text)
-        except ValueError:
-            return [json.loads(entry) for entry in response.text.split("\r\n") if entry != '']
-    else:
-        raise Exception(response.status_code)
+def get_dns(domain: str) -> JsonResponse:
+
+    API = 'https://freeapi.robtex.com/pdns/forward/'
+    
+    try:
+        data = requests.get(f'{API}{domain}')
+        code = data.status_code
+        if code == 200 and data.text != '':
+            info = [json.loads(entry) for entry in data.text.split("\r\n") if entry != '']
+            dnsserv = [r['rrdata'] for r in info if r['rrtype']=='NS']
+            response = JsonResponse({'results': dnsserv})
+        else:
+            response = JsonResponse({'results':f'Error: 404 Not Found'}, status=404)
+    except Exception as e:
+        msg = 'Este servicio no está disponible en este momento:'
+        response = JsonResponse({'results': f'{msg} {e}'}, status=503)
+    finally:
+        return response
