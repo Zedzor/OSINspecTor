10620725bb7d98f9027ce54a50f3c2d9ec06a9bf
nborregan <ignacio.borregan@udc.es>
2021-06-13 01:15:51 +0200

    Arreglado response de la funcionalidad del mail server

--- OSINspecTor/app/functionalities/domain/mx.py
+++ OSINspecTor/app/functionalities/domain/mx.py
@@ -1,15 +1,23 @@
 import json
 import requests
+from django.http import JsonResponse
 
 
-def get_mx(domain):
-    domain_url = "https://freeapi.robtex.com/pdns/forward/"+domain
-    response = requests.get(domain_url)
-    if response.ok:
-        try:
-            return json.loads(response.text)
-        except ValueError:
-            info = [json.loads(entry) for entry in response.text.split("\r\n") if entry != '']
-            return [r['rrdata'] for r in info if r['rrtype']=='MX']
-    else: 
-        raise Exception(response.status_code)
+def get_mx(domain: str) -> JsonResponse:
+
+    API = 'https://freeapi.robtex.com/pdns/forward/'
+
+    try:    
+        data = requests.get(f'{API}{domain}')
+        code = data.status_code
+        if code == 200 and data.text != '':
+                info = [json.loads(entry) for entry in data.text.split("\r\n") if entry != '']
+                mailservers = [r['rrdata'] for r in info if r['rrtype']=='MX']
+                response = JsonResponse({'results': mailservers})
+        else:
+            response = JsonResponse({'results':f'Error: 404 Not Found'}, status=404)
+    except Exception as e:
+        msg = 'Este servicio no está disponible en este momento:'
+        response = JsonResponse({'results': f'{msg} {e}'}, status=503)
+    finally:
+        return response
