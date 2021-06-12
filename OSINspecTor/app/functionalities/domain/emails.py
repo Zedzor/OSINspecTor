6d26fd1b0bb73f5b79c72e0255e7df458de10491
nborregan <ignacio.borregan@udc.es>
2021-06-12 14:24:02 +0200

    Arreglados errores emails :)

--- OSINspecTor/app/functionalities/domain/emails.py
+++ OSINspecTor/app/functionalities/domain/emails.py
@@ -14,8 +14,12 @@ def get_emails(domain):
         query=f"intext:\"@{domain}\""
         lista=[]
         for j in search(query,tld="com", num=20,stop=20,pause=2):
-            page = requests.get(j)
-            lista+=email_on_url(page, domain)
+            if re.search("\.pdf$", j) is None:
+                try:
+                    page = requests.get(j)
+                    lista+=email_on_url(page, domain)
+                except:
+                    pass
         if lista == []:
             sercode=False
             raise Exception("No se encontraron resultados para ese dominio")
