c6260034a14c77cdbde16a156aa75b625dd78ce5
nborregan <ignacio.borregan@udc.es>
2021-06-13 03:03:51 +0200

    Añadida funcionalidad de obtener rango ip de un dominio

---
+++ OSINspecTor/app/functionalities/domain/range.py
@@ -0,0 +1,26 @@
+from socket import gethostbyname as g_host
+import requests
+
+def get_range(domain: str) -> dict:
+
+    API = 'https://rest.db.ripe.net/search.json?query-string='
+    FLAGS = '&type-filter=inetnum&flags=no-referenced&flags=no-irt&source=RIPE'
+
+    try:
+        param = g_host(domain)
+        data = requests.get(f'{API}{param}{FLAGS}')
+        data.status_code
+        if data.status_code == 200:
+            json_res = data.json()
+            json_res = json_res['objects']['object'][0]
+            json_res = json_res['primary-key']['attribute'][0]
+            json_res = json_res['value']
+            results = json_res
+        else:
+            results = f'Error: {data.status_code} {data.reason}'
+        status = data.status_code
+    except Exception as e:
+        results = 'Este servicio no está disponible en este momento:'
+        status = 503
+    finally:
+        return {'results': results, 'status': status}
